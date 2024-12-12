2a9f95378fab37f9843848d48ec094016c94a966
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-12 21:46:01 +0530

    Add files via upload

---
+++ Acceptinglong.java
@@ -0,0 +1,13 @@
+/* 232323454545233333
+90  //output
+232323454545233333   */
+import java.util.*;
+public class Acceptinglong{
+    public static void main(String args[]){
+        long ans=90;
+        Scanner s=new Scanner(System.in);
+        long s1=s.nextLong();
+        System.out.println(ans);
+        System.out.println(s1);
+    }
+}
