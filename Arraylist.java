8b98adbfd149e4f6dcb6c1a4cbde8cb1316cc049
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-12 21:46:28 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,11 @@
+/*  [345, 909, 767]  */
+import java.util.*;
+class Arraylist{
+    public static void main(String args[]){
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(345);
+        a.add(909);
+        a.add(767);
+        System.out.println(a);
+    }
+}
